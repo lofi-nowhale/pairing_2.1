2f3d01461e78cebefca4749f5ce46b50ee976c17
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-06-01 15:29:01 -0600

    Add places to visit

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ arrays = ["Rachel", "Tyler", "Antoine", "Eliza", "Nick"]
 p arrays[2]
 p arrays[-1]
 p arrays[0]
+
+places_to_visit = ["Greece", "Patagonia", "Russia", "Australia", "New Zealand"]
+
+p places_to_visit.include?("Greece")
